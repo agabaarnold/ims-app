c8cea50eabb9e4dca6d1608b3b0396dcdf19b293
Agaba Arnold <agabaarnold23@gmail.com>
2026-06-11 20:38:11 +0300

    feat: add NavMain component for sidebar navigation

---
+++ src/components/sidebar/nav-main.tsx
@@ -0,0 +1,11 @@
+import { SidebarGroup, SidebarGroupLabel } from "../ui/sidebar";
+
+function NavMain() {
+    return (
+        <SidebarGroup>
+            <SidebarGroupLabel className="uppercase">Navigation</SidebarGroupLabel>
+        </SidebarGroup>
+    );
+}
+
+export default NavMain;
