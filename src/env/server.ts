9012c0f97c7894440296191b73b5cfe2bf8a9625
Agaba Arnold <agabaarnold23@gmail.com>
2026-06-11 13:19:21 +0300

    feat: add additional environment variables for authentication and SMTP configuration

--- src/env/server.ts
+++ src/env/server.ts
@@ -2,10 +2,21 @@ import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
 export const serverEnv = createEnv({
-  server: {
-    DATABASE_URL: z.url(),
-    NODE_ENV: z.enum(["development", "production"]).default("production"),
-  },
-  runtimeEnv: process.env,
-  emptyStringAsUndefined: true,
+    server: {
+        DATABASE_URL: z.url(),
+        NODE_ENV: z.enum(["development", "production"]).default("production"),
+        BETTER_AUTH_SECRET: z.string(),
+        BETTER_AUTH_URL: z.url(),
+        GITHUB_CLIENT_ID: z.string(),
+        GITHUB_CLIENT_SECRET: z.string(),
+        GOOGLE_CLIENT_ID: z.string(),
+        GOOGLE_CLIENT_SECRET: z.string(),
+        SMTP_HOST: z.string(),
+        SMTP_PORT: z.coerce.number(),
+        SMTP_USER: z.string(),
+        SMTP_PASS: z.string(),
+        SMTP_FROM: z.string(),
+    },
+    runtimeEnv: process.env,
+    emptyStringAsUndefined: true,
 });
