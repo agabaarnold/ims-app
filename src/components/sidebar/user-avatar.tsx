3fcac137d1a976a2d160773d2d26d26ffdc83f47
Agaba Arnold <agabaarnold23@gmail.com>
2026-06-11 18:37:44 +0300

    feat: add UserAvatar component for displaying user profile information

---
+++ src/components/sidebar/user-avatar.tsx
@@ -0,0 +1,28 @@
+import type { UserWithRole } from "better-auth/plugins";
+import { getInitials } from "#/lib/utils";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+
+
+interface UserAvatarProps {
+    user: UserWithRole;
+}
+
+function UserAvatar({ user }: UserAvatarProps) {
+    const initials = getInitials(user.name);
+
+    return (
+        <>
+            <Avatar>
+                <AvatarImage alt={user.name} src={user.image ?? undefined} />
+                <AvatarFallback className="rounded-lg">{initials}</AvatarFallback>
+            </Avatar>
+
+            <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-medium">{user.name}</span>
+                <span className="truncate text-xs">{user.email}</span>
+            </div>
+        </>
+    );
+}
+
+export default UserAvatar;
