e3364c11080af506662467cd7932d977af96adc2
Agaba Arnold <agabaarnold23@gmail.com>
2026-06-17 18:16:05 +0300

    feat: add FormNumberInput to field components for enhanced numeric input handling

--- src/hooks/use-form.ts
+++ src/hooks/use-form.ts
@@ -1,12 +1,13 @@
 import { createFormHook } from "@tanstack/react-form";
 import { FormCheckbox } from "#/components/form/form-checkbox";
 import { FormInput } from "#/components/form/form-input";
+import FormNumberInput from "#/components/form/form-number-input";
 import { FormPassword } from "#/components/form/form-password";
 import { SubmitButton } from "#/components/form/submit-button";
 import { fieldContext, formContext } from "./use-form-context";
 
 export const { useAppForm } = createFormHook({
-    fieldComponents: { FormCheckbox, FormInput, FormPassword },
+    fieldComponents: { FormCheckbox, FormInput, FormNumberInput, FormPassword },
     fieldContext,
     formComponents: { SubmitButton },
     formContext,
