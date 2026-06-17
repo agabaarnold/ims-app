26a30a6e2a84ba12010411a5ac7db81553bd15bf
Agaba Arnold <agabaarnold23@gmail.com>
2026-06-17 18:35:22 +0300

    feat: add FormTextArea to field components in useAppForm hook

--- src/hooks/use-form.ts
+++ src/hooks/use-form.ts
@@ -3,11 +3,18 @@ import { FormCheckbox } from "#/components/form/form-checkbox";
 import { FormInput } from "#/components/form/form-input";
 import FormNumberInput from "#/components/form/form-number-input";
 import { FormPassword } from "#/components/form/form-password";
+import FormTextArea from "#/components/form/form-text-area";
 import { SubmitButton } from "#/components/form/submit-button";
 import { fieldContext, formContext } from "./use-form-context";
 
 export const { useAppForm } = createFormHook({
-    fieldComponents: { FormCheckbox, FormInput, FormNumberInput, FormPassword },
+    fieldComponents: {
+        FormCheckbox,
+        FormInput,
+        FormNumberInput,
+        FormPassword,
+        FormTextArea,
+    },
     fieldContext,
     formComponents: { SubmitButton },
     formContext,
