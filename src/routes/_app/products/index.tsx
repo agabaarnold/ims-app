19f68e132571bd6d19a41ee34e2cbca9add9da9c
Agaba Arnold <agabaarnold23@gmail.com>
2026-06-17 15:23:58 +0300

    feat: refactor ProductPage to use Link directly instead of Button for new product navigation

--- src/routes/_app/products/index.tsx
+++ src/routes/_app/products/index.tsx
@@ -3,7 +3,7 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { Suspense } from "react";
 import { z } from "zod";
-import { Button } from "#/components/ui/button";
+import { buttonVariants } from "#/components/ui/button";
 import { productColumns } from "#/features/products/components/product-columns";
 import { ProductTable } from "#/features/products/components/product-table";
 import { getProductsQuery } from "#/features/products/functions/queries";
@@ -14,14 +14,13 @@ export const Route = createFileRoute("/_app/products/")({
         page: search.page,
         pageSize: search.pageSize,
     }),
-    loader: ({ context: { queryClient }, deps }) => {
+    loader: ({ context: { queryClient }, deps }) =>
         queryClient.ensureQueryData(
             getProductsQuery({
                 page: deps.page,
                 pageSize: deps.pageSize,
             })
-        );
-    },
+        ),
     validateSearch: z.object({
         page: z.number().int().min(1).default(1).catch(1),
         pageSize: z.number().int().min(5).max(100).default(10).catch(10),
@@ -39,14 +38,14 @@ function ProductPage() {
             <div className="flex items-center justify-between">
                 <h1 className="font-semibold text-3xl">Products</h1>
 
-                <Button variant="default">
-                    <Link
-                        className="flex items-center gap-1"
-                        to="/products/new"
-                    >
-                        <IconPlus /> <span>New Product</span>
-                    </Link>
-                </Button>
+                <Link
+                    className={buttonVariants({
+                        variant: "default",
+                    })}
+                    to="/products/new"
+                >
+                    <IconPlus /> <span>New Product</span>
+                </Link>
             </div>
 
             <Suspense fallback={<div>Loading products...</div>}>
