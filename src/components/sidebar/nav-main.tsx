9d02a941f9de2120e5b92d01f4e5070dc8b06e9e
Agaba Arnold <agabaarnold23@gmail.com>
2026-06-23 17:43:06 +0300

    feat: enhance navigation sidebar with additional menu items and icons

--- src/components/sidebar/nav-main.tsx
+++ src/components/sidebar/nav-main.tsx
@@ -1,6 +1,19 @@
-import { IconHome, type TablerIcon } from "@tabler/icons-react";
-import type { LinkOptions } from "@tanstack/react-router";
-import { SidebarGroup, SidebarGroupLabel, SidebarMenu } from "../ui/sidebar";
+import {
+    IconBasketPlus,
+    IconBuildingWarehouse,
+    IconCategory,
+    IconHome,
+    IconPackages,
+    type TablerIcon,
+} from "@tabler/icons-react";
+import { Link, type LinkOptions } from "@tanstack/react-router";
+import {
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "../ui/sidebar";
 
 interface NavItems {
     icon: TablerIcon;
@@ -8,7 +21,13 @@ interface NavItems {
     url: LinkOptions["to"];
 }
 
-const items: NavItems[] = [{ title: "Dashboard", url: "/", icon: IconHome }];
+const items: NavItems[] = [
+    { title: "Dashboard", url: "/", icon: IconHome },
+    { title: "Products", url: "/products", icon: IconPackages },
+    { title: "Warehouses", url: "/warehouses", icon: IconBuildingWarehouse },
+    { title: "Categories", url: "/categories", icon: IconCategory },
+    { title: "Suppliers", url: "/suppliers", icon: IconBasketPlus },
+];
 
 function NavMain() {
     return (
@@ -17,7 +36,26 @@ function NavMain() {
                 Navigation
             </SidebarGroupLabel>
 
-            <SidebarMenu>{}</SidebarMenu>
+            <SidebarMenu className="space-y-1.5">
+                {items.map((item) => (
+                    <SidebarMenuItem key={item.title}>
+                        <SidebarMenuButton
+                            render={
+                                <Link
+                                    activeProps={{
+                                        className:
+                                            "bg-primary text-secondary hover:text-secondary hover:bg-primary",
+                                    }}
+                                    to={item.url}
+                                >
+                                    <item.icon />
+                                    <span>{item.title}</span>
+                                </Link>
+                            }
+                        />
+                    </SidebarMenuItem>
+                ))}
+            </SidebarMenu>
         </SidebarGroup>
     );
 }
